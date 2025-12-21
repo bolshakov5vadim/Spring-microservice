1930343069ecfdd66403eed53a55d8003ef2ee4a
bolshakov5vadim <99659244+bolshakov5vadim@users.noreply.github.com>
2025-12-21 16:29:45 +0500

    Update MyConfig.java

--- java/com/example/configuration/MyConfig.java
+++ java/com/example/configuration/MyConfig.java
@@ -3,6 +3,7 @@ package com.example.configuration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 // По умолчанию кросс-доменные запросы запрещены. Мы их разрешаем.
@@ -10,7 +11,7 @@ public class MyConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/auth", "/secret")
+        registry.addMapping("/*")
             .allowedOrigins("*")
             .allowedMethods("GET", "POST", "UPDATE", "DELETE", "OPTIONS")
             .allowedHeaders("*")
