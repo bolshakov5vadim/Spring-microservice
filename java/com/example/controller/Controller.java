a4d12b04e464df8b0d734c7a2ce8b40e4d1fa03e
bolshakov5vadim <99659244+bolshakov5vadim@users.noreply.github.com>
2025-12-21 16:30:23 +0500

    Update Controller.java

--- java/com/example/controller/Controller.java
+++ java/com/example/controller/Controller.java
@@ -1,4 +1,4 @@
-package com.example.demo.controller;
+package com.example.controller;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,8 +11,8 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import com.example.demo.entity.MyEntity;//название файла. подключили сущность 
-import com.example.demo.repository.MyRepository;//название файла. подключили репозиторий
+import com.example.entity.MyEntity;//название файла. подключили сущность 
+import com.example.repository.MyRepository;//название файла. подключили репозиторий
 
 @RestController//аннотация, что делает код контроллером
 @RequestMapping("/employees")//путь для всех методов. здесь пойдет микросервис
