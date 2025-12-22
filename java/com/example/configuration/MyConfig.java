63ad95021c21433a9da8b12be210160c0bb2606e
bolshakov5vadim <99659244+bolshakov5vadim@users.noreply.github.com>
2025-12-22 22:08:55 +0500

    Update MyConfig.java

--- java/com/example/configuration/MyConfig.java
+++ java/com/example/configuration/MyConfig.java
@@ -11,7 +11,7 @@ public class MyConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/*")
+        registry.addMapping("/**")
             .allowedOrigins("*")
             .allowedMethods("GET", "POST", "UPDATE", "DELETE", "OPTIONS")
             .allowedHeaders("*")
